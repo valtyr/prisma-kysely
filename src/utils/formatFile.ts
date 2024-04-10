bfc5eaed77cbaa86f7ab7b68eb386c8d91506494
jkesuma <wijayakesuma@payfirstsolutions.com>
2024-04-10 11:20:37 -0400

    fix: Prettier v3 and up resolveConfig broken

--- src/utils/formatFile.ts
+++ src/utils/formatFile.ts
@@ -1,11 +1,21 @@
+import type { Options } from "prettier";
+
 export const formatFile = async (content: string) => {
   // If user has prettier, we find their config and
   // format. Otherwise we don't alter the file.
 
   try {
     const { default: prettier } = await import("prettier");
+    
+    const prettierMajorVersion = parseInt(prettier.version.split('.')[0]);
+    let config: Options | null = null;
 
-    const config = await prettier.resolveConfig(process.cwd());
+    if (prettierMajorVersion>=3) {
+      config = await prettier.resolveConfig(process.cwd() + '/.prettierrc');
+    } else {
+      config = await prettier.resolveConfig(process.cwd());
+    }
+    
     if (!config) return content;
 
     const formatted = prettier.format(content, {
