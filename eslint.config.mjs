3905d2dbf3575de560ba98389efc75c7c28b58af
Alistair Smith <hi@alistair.sh>
2026-03-14 12:31:25 -0700

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,5 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
 import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores([
@@ -21,18 +8,9 @@ export default defineConfig([
     "./eslint.config.mjs",
     "./vitest.config.mts",
   ]),
+  tseslint.configs.recommended,
   {
-    extends: compat.extends("plugin:@typescript-eslint/recommended"),
-
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-
     languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: "script",
-
       parserOptions: {
         project: "./tsconfig.json",
       },
